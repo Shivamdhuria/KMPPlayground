944dc38d1d5d301606440e926f79268b8f86fc17
shiv <shivam_dhuria@condenast.com>
2022-02-07 15:02:54 +0530

    unified ios targets

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -69,6 +69,8 @@ kotlin {
         val jsTest by getting
         val nativeMain by getting
         val nativeTest by getting
+
+
         val androidMain by getting {
             dependencies {
                 implementation("com.google.android.material:material:1.2.1")
@@ -81,12 +83,25 @@ kotlin {
         }
         val macosX64Main by getting
         val macosX64Test by getting
-        val iosArm32Main by getting
-        val iosArm32Test by getting
-        val iosArm64Main by getting
-        val iosArm64Test by getting
+
         val iosX64Main by getting
+        val iosArm64Main by getting
+        //val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            //iosSimulatorArm64Main.dependsOn(this)
+        }
         val iosX64Test by getting
+        val iosArm64Test by getting
+        //val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            //iosSimulatorArm64Test.dependsOn(this)
+        }
     }
 }
 android {
