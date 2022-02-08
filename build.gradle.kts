c0bc007633e7e8b23c33b48aedaecc680ad29756
shiv <shivam_dhuria@condenast.com>
2022-02-08 14:26:51 +0530

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 @Suppress("AndroidGradlePluginVersion") buildscript {
+
+    val kotlinVersion: String by project
+    println(kotlinVersion)
+
     repositories {
         gradlePluginPortal()
         jcenter()
@@ -6,7 +10,7 @@
         mavenCentral()
     }
 
-    val kotlinVersion = "1.6.10"
+//    val kotlinVersion = "1.6.10"
     val sqlDelightVersion: String by project
     val hilt_version = "2.38.1"
 
@@ -14,8 +18,15 @@
         classpath("com.android.tools.build:gradle:7.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
+//        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
         classpath( "com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
+
+        with(Deps.Gradle) {
+            classpath(sqlDelight)
+            classpath(shadow)
+            classpath(gradleVersionsPlugin)
+            classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutinesVersion}")
+        }
     }
 }
 
